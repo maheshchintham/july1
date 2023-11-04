c401f348e39e53b4999dd17a58f2b59e63fd1be7
maheshchintham <maheshchintham400@gmail.com>
2023-11-04 20:49:11 +0530

    mehra

---
+++ collectionTest.java
@@ -0,0 +1,36 @@
+
+package arrylistt;
+
+import java.util.ArrayList;
+
+public class empptest {
+
+	public static void main(String[] args) {
+
+		empp e=new empp();
+		empp e1=new empp();
+
+      ArrayList a=new ArrayList();
+
+		a.add(e);
+		a.add(e1);
+
+
+
+		System.out.println(a.size());
+		System.out.println(a);
+
+    ArrayList a1=new ArrayList();
+
+		a1.add(e);
+		a1.add(e1);
+		a1.addAll(a);
+
+		System.out.println(a1.size());
+		System.out.println(a1);
+
+
+	}
+
+}
+
